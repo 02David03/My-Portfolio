72be3a8256c40ee93510b096c861734c8e236dba
David de Almeida Cunha <daviid.0203@gmail.com>
2023-11-09 19:50:40 -0300

    feat: Header without funcional links

--- src/shared-components/header/header.jsx
+++ src/shared-components/header/header.jsx
@@ -1,17 +1,33 @@
+import { motion } from "framer-motion";
+import { useState } from "react";
+
 function Header() {
+  const [x, setX] = useState(0);
+  const [scaleX, setScaleX] = useState(1);
 
   return(
-    <header className="bg-black flex justify-center shadow-white shadow-md">
+    <header className="bg-black flex justify-center">
       <div className="container flex justify-between items-center py-2">
-        <h2 className="text-white"> D.A </h2>
-        <nav className="flex flex-col items-end">
-          <div className="text-primary text-3xl flex gap-6">
-            <a className="" href="#"> About me </a>
-            <a href="#"> Portfolio </a>
+        <a href="#" className="rounded-full bg-white px-2 cursor-pointer">
+          <h2 className="text-black"> D.A </h2>
+        </a>
+        <nav className="flex flex-col items-end gap-1">
+          <div className="text-primary text-4xl flex gap-6">
+            <a className="hover:font-bold" 
+              onMouseEnter={() => {setScaleX(0.5); setX(-70) }}
+              onMouseLeave={() => {setScaleX(1); setX(0)}}
+              href="#"> About me </a>
+            <a className="hover:font-bold" 
+              onMouseEnter={() => {setScaleX(0.5); setX(100) }}
+              onMouseLeave={() => {setScaleX(1); setX(0)}}
+              href="#"> Portfolio </a>
           </div>
-          <div className="flex w-64 h-1 bg-primary rounded"></div>
+          <motion.div 
+            className="flex w-80 h-1 bg-primary rounded"
+            animate={{x, scaleX}}
+            transition={{ type: 'spring' }}
+          />
         </nav>
-        
       </div>
     </header>
   )
