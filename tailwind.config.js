66fdd275a73bb768c63e51f1296fb5f03a12fe5c
David de Almeida Cunha <david.cunha@lais.huol.ufrn.br>
2023-11-14 20:16:30 -0300

    style: animations on welcome section

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,6 @@ module.exports = {
       'secondary': '#88159B',
       'terciary': '#F3AD9B'
     },
-    borderWidth: {
-      '16': '16px',
-      '24': '24px',
-    },
     extend: {},
   },
   plugins: [],
